a56c8863087ad4b3fe8da80001645f0b8f093498
Blancahr <143891402+Blancahr@users.noreply.github.com>
2025-05-21 14:05:49 -0600

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterModule, HttpClientModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'catalogo-web';
+}
